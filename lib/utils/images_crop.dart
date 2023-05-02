be900228263b40e5dc4950ab2f8b1d92b9eb9cc2
Lucas <lucas.moreno.olivos@gmail.com>
2023-05-01 21:23:46 -0500

    a

--- lib/utils/images_crop.dart
+++ lib/utils/images_crop.dart
@@ -1,5 +1,14 @@
 final Map<String, String> images = {
-  "Alimentarios": 'assets/images/alimentario.jpg',
-  "Ornamentales": 'assets/images/ornamental.png',
-  "Textiles": 'assets/images/textil.jpg',
+  "Arroz": 'assets/images/alimentario.jpg',
+  "Tomate": 'assets/images/alimentario.jpg',
+  "Rabanito": 'assets/images/alimentario.jpg',
+  "Maiz": 'assets/images/alimentario.jpg',
+  "Algodon": 'assets/images/ornamental.png',
+  "Canamo": 'assets/images/ornamental.png',
+  "Seda": 'assets/images/ornamental.png',
+  "Lino": 'assets/images/ornamental.png',
+  "Helechos": 'assets/images/textil.jpg',
+  "Rosas": 'assets/images/textil.jpg',
+  "Hortensias": 'assets/images/textil.jpg',
+  "Orquideas": 'assets/images/textil.jpg',
 };
